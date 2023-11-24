0cebac1b45162549b5e5f4e49849675a81bfe868
steffany cobos <cobosleandra2@gmail.com>
2023-11-24 12:20:14 -0300

    Enviorment modified

--- proyectoFinal/AppCoder/models.py
+++ proyectoFinal/AppCoder/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Familia(models.Model):
+    nombre= models.CharField(max_length=15)
+    apellido= models.CharField(max_length=15)
+    fecha=models.DateField(max_length=15)
--- proyectoFinal/AppCoder/views.py
+++ proyectoFinal/AppCoder/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Template, Context
+from AppCoder.models import Familia
 
 # Create your views here.
-def inicio_view(xx):
-    return HttpResponse("Bienvenidos!!!!!!!!!!!!!!")
+def inicio_view(self):
+    user= Familia(nombre='Leandra',apellido='Cobos', fecha='1994-12-17')
+    user.save()
+    userTexto= f'Nombre: {user.nombre}, Apellido: {user.apellido}, Nacimiento: {user.fecha}'
+    return HttpResponse(userTexto)
 
 def probandoTemplate(xx):
     mihtml=open(r'C:\Users\Steffany Cobos\Desktop\python_proyecto\proyectoFinal\AppCoder\templates\padre.html')
